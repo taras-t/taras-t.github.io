3c6f2e69c8a36bd7a296304181ad6fee980aa76e
taras-t <62204071+taras-t@users.noreply.github.com>
2020-03-27 16:55:29 +0200

    Update forms.js

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,16 +1,16 @@
 function writeNewForm(){
    var current = new Date();
-   document.writeln("<form action=\"result.html\" method=\"GET\" name=\"new_form\">");
-   document.writeln("<p>Назва: <input name=\"name\" type=\"text\" size=\"20\" value=\"",getParam("name") , "\"> ", help(0), "</p>");
-   document.writeln("<p>Вид птиці: <select name=\"type\">");
+   document.writeln("<form action=\"result.html\" method=\"GET\" name=\"new_form\"><table cellspacing=\"10\">");
+   document.writeln("<tr><td>Назва: <input name=\"name\" type=\"text\" size=\"20\" value=\"",getParam("name") , "\"> ", help(0), "</td></tr>");
+   document.writeln("<tr><td>Вид птиці: <select name=\"type\">");
    document.writeln("<option selected value=\"0\">Курка</option>");
    document.writeln("<option value=\"1\">Качка</option>");
    document.writeln("<option value=\"2\">Індокачка</option>");
    document.writeln("<option value=\"3\">Гуска</option>");
    document.writeln("<option value=\"4\">Індик</option>");
    document.writeln("<option value=\"5\">Перепілка</option>");
-   document.writeln("</select> ", help(1), "</p>");
-   document.writeln("<p>Дата закладки:</p><p>Число: <select name=\"date\">");
+   document.writeln("</select> ", help(1), "</td></tr>");
+   document.writeln("<tr><td>Дата закладки:</td></tr><tr><td>Число: <select name=\"date\">");
    for(var i = 1; i <= 31; i++){
       if(i == current.getDate()){
             document.writeln("<option selected>", i, "</option>");
@@ -19,8 +19,8 @@ function writeNewForm(){
             document.writeln("<option>", i, "</option>");
       }
    }
-   document.writeln("</select> ", help(2), "</p>");
-   document.writeln("Місяць: <select name=\"month\">");
+   document.writeln("</select> ", help(2), "</td></tr>");
+   document.writeln("<tr><td>Місяць: <select name=\"month\">");
    for(var i = 1; i <= 12; i++){
       if(i == current.getMonth() +1){
             document.writeln("<option selected value=\"",i , "\">", getMonthName(i), "</option>");
@@ -29,14 +29,14 @@ function writeNewForm(){
             document.writeln("<option value=\"",i , "\">", getMonthName(i), "</option>");
       }
    }
-   document.writeln("</select> ", help(3), "</p>");
+   document.writeln("</select> ", help(3), "</td></tr>");
 
-   document.writeln("<p>Рік: <select name=\"year\">");
+   document.writeln("<tr><td>Рік: <select name=\"year\">");
    document.writeln("<option>", current.getFullYear() - 1, "</option>");
    document.writeln("<option selected>", current.getFullYear(), "</option>");
    document.writeln("<option>", current.getFullYear() + 1, "</option>");
-   document.writeln("</select> ", help(4), "</p>");
-   document.writeln("<input type=\"submit\" value=\"Ok\"> <input type=\"reset\" value=\"Скинути\"> ", help(5), "</form>");
+   document.writeln("</select> ", help(4), "</td></tr>");
+   document.writeln("<tr><td><input type=\"submit\" value=\"Ok\"> <input type=\"reset\" value=\"Скинути\"> ", help(5), "</td></tr></table></form>");
 }
 function writeBackForm(){
    document.writeln("<br><form name=\"back_form\">");
